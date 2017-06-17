defd630a13629379f236a54dc1eddaffae347ab3
ajthebear <aj.kuemmel@gmail.com>
2017-06-17 13:22:22 -0400

    first commit

--- anotherPostcard/ViewController.swift
+++ anotherPostcard/ViewController.swift
@@ -6,9 +6,20 @@
 //  Copyright © 2017 BrokenGlassKids. All rights reserved.
 //
 
+
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+ 
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal
+)
+    
 }
 
+}
