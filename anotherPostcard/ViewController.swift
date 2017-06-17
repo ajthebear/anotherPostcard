21f8dca9086b8ca389055805c665a760a90189fc
ajthebear <aj.kuemmel@gmail.com>
2017-06-17 15:36:15 -0400

    Name Label Functionality

--- anotherPostcard/ViewController.swift
+++ anotherPostcard/ViewController.swift
@@ -6,21 +6,18 @@
 //  Copyright © 2017 BrokenGlassKids. All rights reserved.
 //
 
-
 import UIKit
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController{
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameMessageLabel: UILabel!
+  
  
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,15 +31,23 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // reveals hidden label to the left of stamp that uses Enter Message test string, changes color of the hidden lable to red. Removes text from Enter Message, and sends keyboard away.
         messageLabel.hidden = false
+        nameMessageLabel.hidden = false
+        
         messageLabel.text = enterMessageTextField.text
+        nameMessageLabel.text = enterNameTextField.text
+        
         messageLabel.textColor = UIColor.redColor()
+        nameMessageLabel.textColor = UIColor.redColor()
         
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.text = ""
         
-        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal
-)
+        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     
+
 }
 
 }
